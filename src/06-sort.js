4360987636055112f88176c72fe3226f79de572b
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-05-01 18:51:49 -0400

    Finish sortByRuntimeAscending in 06-sort.js.

--- src/06-sort.js
+++ src/06-sort.js
@@ -5,7 +5,7 @@
 
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
-const exampleSongData = require("../data/songs");
+const exampleSongData = require('../data/songs');
 // Do not change the line above.
 
 /**
@@ -13,8 +13,12 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
-
+function sortByRuntimeAscending(exampleSongData) {
+	return exampleSongData.sort(
+		(a, b) => a.runtimeInSeconds - b.runtimeInSeconds
+	);
+}
+console.log(sortByRuntimeAscending(exampleSongData));
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
  *
@@ -23,7 +27,7 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(exampleSongData) {}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,10 +37,10 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(exampleSongData) {}
 
 module.exports = {
-  sortByRuntimeAscending,
-  sortByArtistNameDescending,
-  sortBySongTitleAscending,
+	sortByRuntimeAscending,
+	sortByArtistNameDescending,
+	sortBySongTitleAscending,
 };
