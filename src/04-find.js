a0d726b0237dfe1c9e3869b8a6c09e8c6f89ed78
Smille007 <smille007@gmai.com>
2023-05-02 16:53:35 -0400

    done

--- src/04-find.js
+++ src/04-find.js
@@ -13,21 +13,31 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+  const pinkElephants = songs.find(song => song.title === "Pink Elephants" && song.artist === "Timestreet");
+  return pinkElephants || null;
+}
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+  const firstSong = songs.find(song => song.runtimeInSeconds < 180 && song.artist);
+  return firstSong || null;
+}
+
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+  const firstTitleTrack = songs.find(song => song.title === song.album);
+  return firstTitleTrack || null;
+}
 
 module.exports = {
   findPinkElephantsByTimestreet,
