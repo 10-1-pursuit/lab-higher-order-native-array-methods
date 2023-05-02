f15743ba179d60105531818bbf1c53a28a49db79
ShakaPursuit <shakalivngstone@pursuit.org>
2023-05-02 12:34:28 -0400

    working on second one

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -1,70 +1,3 @@
-const songs = [
-  {
-    title: "Berlin Tsukin",
-    album: "Bi-To Te-Pu",
-    artist: "Taiyo Ky",
-    runtimeInSeconds: 193,
-  },
-  {
-    title: "Up",
-    album: "Bluewerks Vol. 1: Up Down Left Right",
-    artist: "Sebastian Kamae",
-    runtimeInSeconds: 120,
-  },
-  {
-    title: "Daylight",
-    album: "Bluewerks Vol. 1: Up Down Left Right",
-    artist: "hiyasu",
-    runtimeInSeconds: 154,
-  },
-  {
-    title: "Dormancy",
-    album: "Dormancy",
-    artist: "Arcade Portal",
-    runtimeInSeconds: 126,
-  },
-  {
-    title: "Pineapple Jam",
-    album: "Bluewerks Vol. 1: Up Down Left Right",
-    artist: "Saib",
-    runtimeInSeconds: 186,
-  },
-  {
-    title: "Samui Sunrise",
-    album: "Samui Sunrise",
-    artist: "Saib",
-    runtimeInSeconds: 204,
-  },
-  {
-    title: "Regular Guy",
-    album: "Regular Guy",
-    artist: "Peanut",
-    runtimeInSeconds: 215,
-  },
-  {
-    title: "In the Middle of Nowhere",
-    album: "It's OK",
-    artist: "Soulaced",
-    runtimeInSeconds: 123,
-  },
-  {
-    title: "Pink Elephants",
-    album: "Waterfalls",
-    artist: "Timestreet",
-    runtimeInSeconds: 171,
-  },
-  {
-    title: "Kyona",
-    album: "Kyona",
-    artist: "Two Sleepy",
-    runtimeInSeconds: 188,
-  },
-];
-
-module.exports = songs;
-//console.log(songs.title)
-
-
 
 
 
@@ -82,19 +15,15 @@ const exampleSongData = require("../data/songs");
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
+//const printAllSongTitles = exampleSongDatas.forEach((songs)=>{console.log([songs.title])})
+console.log("===================first function")
 
-const printAllSongTitles = songs.forEach((songs)=>{console.log(songs.title)})
-// function printAllSongTitles(songs) {
-//   songs.forEach(songs){
-//     console.log(songs.title)
-    
-//   }
-// }
-
-// printAllSongTitles()
-
-
-
+function printAllSongTitles(songs){     
+  
+  const printAllSongTitles = songs.forEach((songs)=>{console.log(songs.title)})
+      }
+  printAllSongTitles(exampleSongData)
+  
 
 
 /**
@@ -107,18 +36,33 @@ const printAllSongTitles = songs.forEach((songs)=>{console.log(songs.title)})
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-//function printSongDetails(songs) {}
+
+//const printSongDetails = exampleSongData.forEach((songs)=>{console.log(songs.title+" by "+songs.artist)})
+console.log("========================SECOND FUNCTION")
+function printSongDetails(songs) {
+
 const printSongDetails = songs.forEach((songs)=>{console.log(songs.title+" by "+songs.artist)})
+        }
+        printSongDetails(exampleSongData)
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-//function printSongTitlesOverThreeMinutes(songs) {}
-const printSongTitlesOverThreeMinutes = songs.forEach((songs)=>{if (songs.runtimeInSeconds>180)(console.log(songs.title))})
+console.log("=========================3rd FUNCTION")
 
+//const printSongTitlesOverThreeMinutes = exampleSongData.forEach((songs)=>{if (songs.runtimeInSeconds>180)(console.log(songs.title))})
+
+
+//console.log("============================3rdFunctionClone")
+
+function printSongTitlesOverThreeMinutes(songs) {
+const printSongTitlesOverThreeMinutes = songs.forEach((songs)=>{if (songs.runtimeInSeconds>180)(console.log(songs.title))})
+}
+printSongTitlesOverThreeMinutes(exampleSongData)
 module.exports = {
  printAllSongTitles,
   printSongDetails,
   printSongTitlesOverThreeMinutes,
 };
+
